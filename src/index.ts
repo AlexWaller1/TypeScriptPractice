34ce82b723c8180f98a3ea56d01d714ce25efb20
Alex Waller <alex@artofalexwaller.com>
2022-06-13 21:29:12 -0400

    basicTypes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 let id: number = 5;
 
+console.log("ID:", id);
+
+// Basic Types
+
+let num1: number = 37;
+// number
+
+let mellonTech: string = "Mellon-Tech";
+// string
+
+let isDachshund: boolean = true;
+// boolean
