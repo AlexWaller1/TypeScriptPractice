defd5a444fcd3b94b11ba7e98c6ad87999d91071
Alex Waller <alex@artofalexwaller.com>
2022-06-13 21:38:22 -0400

    array dataTypes

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,18 @@ let anyThing: any = "anything";
 anyThing = 31;
 // will not error out when data type is changed
 // because it is an 'any' data type
+
+let randomNum : number;
+// can leave undeclared
+randomNum = 49;
+// then declare it later as the correct data type
+
+let numsArray: number[] = [1, 2, 3, 4, 5];
+// specify data type of array
+// can't push new elements not of that data type
+
+let strings1: string[] = ["a", "b", "c", "d", "e"];
+// specify array of strings
+
+let booleans1: boolean[] = [true, true, false, false];
+// specify array of booleans
