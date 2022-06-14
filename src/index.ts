96db45e83f8ac3c555d68543959d6107b1d10bef
Alex Waller <alex@artofalexwaller.com>
2022-06-13 21:31:57 -0400

    dataTypes

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,9 @@ let mellonTech: string = "Mellon-Tech";
 
 let isDachshund: boolean = true;
 // boolean
+
+let anyThing: any = "anything";
+// any
+anyThing = 31;
+// will not error out when data type is changed
+// because it is an 'any' data type
